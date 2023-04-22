3044a742463161ff5058e700d684b3cdfd683e57
kimkh05 <nestjs01@naver.com>
2023-04-23 00:20:51 +0900

    chore: file location

--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,21 @@
+// image
 export { default as ProfileImage } from './image/profile_image.jpg';
-export { default as GithubIcon } from './image/github_icon.svg';
-export { default as PhoneIcon } from './image/phone_icon.svg';
-export { default as EmailIcon } from './image/email_icon.svg';
 
-export { default as ReactIcon } from './image/react.png';
-export { default as JavascriptIcon } from './image/javascript.png';
-export { default as TypescriptIcon } from './image/typescript.png';
-export { default as RecoilIcon } from './image/recoil.png';
-export { default as NextjsIcon } from './image/nextjs.png';
-export { default as Github } from './image/github.svg';
-export { default as ReduxIcon } from './image/redux.png';
-export { default as NestjsIcon } from './image/nestjs.svg';
+// image/icon
+export { default as GithubIcon } from './image/icon/github_icon.svg';
+export { default as PhoneIcon } from './image/icon/phone_icon.svg';
+export { default as EmailIcon } from './image/icon/email_icon.svg';
+
+// image/language
+export { default as ReactIcon } from './image/language/react.png';
+export { default as JavascriptIcon } from './image/language/javascript.png';
+export { default as TypescriptIcon } from './image/language/typescript.png';
+export { default as RecoilIcon } from './image/language/recoil.png';
+export { default as NextjsIcon } from './image/language/nextjs.png';
+export { default as Github } from './image/language/github.svg';
+export { default as ReduxIcon } from './image/language/redux.png';
+export { default as NestjsIcon } from './image/language/nestjs.svg';
+
+//image/activity
+export { default as CSStudyLogo } from './image/activity/cs_study.png';
+export { default as UPLogo } from './image/activity/dsm-up.png';
