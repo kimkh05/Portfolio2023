9b796c4d1eedb9184f2db96cf6c38d9710d536d6
kimkh05 <nestjs01@naver.com>
2023-04-22 20:14:33 +0900

    feat: init image

---
+++ assets/index.ts
@@ -0,0 +1,4 @@
+export { default as ProfileImage } from './image/profile_image.png';
+export { default as GithubIcon } from './image/github_icon.svg';
+export { default as PhoneIcon } from './image/phone_icon.svg';
+export { default as EmailIcon } from './image/email_icon.svg';
