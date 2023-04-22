b09e9403875351a7a478c774ab7d8b0531c0b209
kimkh05 <nestjs01@naver.com>
2023-04-22 23:25:37 +0900

    feat: init image

--- assets/index.ts
+++ assets/index.ts
@@ -2,3 +2,12 @@ export { default as ProfileImage } from './image/profile_image.png';
 export { default as GithubIcon } from './image/github_icon.svg';
 export { default as PhoneIcon } from './image/phone_icon.svg';
 export { default as EmailIcon } from './image/email_icon.svg';
+
+export { default as ReactIcon } from './image/react.png';
+export { default as JavascriptIcon } from './image/javascript.png';
+export { default as TypescriptIcon } from './image/typescript.png';
+export { default as RecoilIcon } from './image/recoil.png';
+export { default as NextjsIcon } from './image/nextjs.png';
+export { default as Github } from './image/github.svg';
+export { default as ReduxIcon } from './image/redux.png';
+export { default as NestjsIcon } from './image/nestjs.svg'
