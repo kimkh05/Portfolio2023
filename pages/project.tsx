0b0f70a3074d3a7f004aace8353f66ccdeaa2b87
kimkh05 <nestjs01@naver.com>
2023-04-23 14:29:21 +0900

    feat: project page

---
+++ pages/project.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/common/header';
+import Frame from '@/components/frame';
+import { PageList } from '@/utils/constants/pagelist';
+
+const Project = () => {
+  return (
+    <Frame status="참여한 프로젝트">
+      <span>Test</span>
+    </Frame>
+  );
+};
+
+export default Project;
