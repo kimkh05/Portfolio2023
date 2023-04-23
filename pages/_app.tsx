0ff53b1ad19f88a2c87fe0bb6e49c838a310ddb5
kimkh05 <nestjs01@naver.com>
2023-04-23 15:25:51 +0900

    fix: recoilroot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from 'next/app';
 import StyleProvider from '@/styles';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <StyleProvider>
       <ToastContainer />
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </StyleProvider>
   );
 }
