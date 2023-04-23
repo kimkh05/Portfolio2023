1245b7d832d31f286cb03d9014108cbbaaea4da2
kimkh05 <nestjs01@naver.com>
2023-04-23 11:30:26 +0900

    fix: font

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,13 +1,14 @@
 import { css } from '@emotion/react';
 
 const GlobalStyle = css`
+  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css');
   * {
     margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
     border: 0;
-    font-family: 'Noto Sans';
+    font-family: 'Pretendard', sans-serif;
     text-decoration: none;
   }
 `;
