6d61ea9c707abc7e8ee08f0ea594e3b556690652
kimkh05 <nestjs01@naver.com>
2023-04-24 17:13:23 +0900

    feat: portfolio title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,16 @@ import StyleProvider from '@/styles';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <StyleProvider>
       <ToastContainer />
       <RecoilRoot>
+        <Head>
+          <title>KyeongHo's Portfolio</title>
+        </Head>
         <Component {...pageProps} />
       </RecoilRoot>
     </StyleProvider>
